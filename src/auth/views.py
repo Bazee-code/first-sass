2848aee41491d3c70b2bddfb6af164d2a531ff3b
Bazee-code <eugeneobazee2@gmail.com>
2024-07-09 12:53:01 +0300

    Add register view and template

--- src/auth/views.py
+++ src/auth/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth import login, authenticate
 
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
 # Create your views here.
 
 def login_view(request, *args, **kwargs):
@@ -18,3 +22,22 @@ def login_view(request, *args, **kwargs):
                 return redirect('/')
         
     return render(request, 'auth/login.html',{})
+
+def register_view(request, *args, **kwargs):
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+
+        username_exists = User.objects.filter(username__iexact = username).exists()
+        email_exists = User.objects.filter(email__iexact = email)
+
+        print('user exists',username_exists)
+        print('email exists',email_exists)
+        
+        try:
+            User.objects.create_user(username, email, password)
+        except:
+            pass
+        
+    return render(request,'auth/register.html',{})
